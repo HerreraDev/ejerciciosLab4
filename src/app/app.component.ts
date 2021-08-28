85e80b44c97275916fbb53d2736cba343ee67efd
martin <martinherrera5000@hotmail.com>
2021-08-28 19:16:44 -0300

    ej2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,21 +10,5 @@ import { Usuario } from './clases/usuario';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ejercicio-one';
-  edadUno ='';
-  edadDos = '';
-  prom = '';
-  suma = '';
-
-  limpiar(){
-    this.edadUno = '';
-    this.edadDos = '';
-    this.prom = '';
-    this.suma = '';
-  }
-
-  calcular(){
-    this.suma= (parseInt(this.edadUno)+parseInt(this.edadDos)).toString();
-    this.prom= (parseInt(this.suma)/2).toString();
-  }
+  constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Ejercicio2Component } from './ejercicio2/ejercicio2.component';
+import { Ejercicio1Component } from './ejercicio1/ejercicio1.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const rutas: Routes = [
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo: 'home'
+  },
+  {
+    path:'home',
+    component: HomeComponent
+  },
+  {
+    path:'ej1',
+    component: Ejercicio1Component
+  },
+  {
+    path:'ej2',
+    component: Ejercicio2Component
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Ejercicio2Component,
+    Ejercicio1Component,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(rutas)
   ],
   providers: [],
   bootstrap: [AppComponent]

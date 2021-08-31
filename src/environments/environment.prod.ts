fc579926e68f6a9f8cf68ab52a3df5d4619cd15f
martin <martinherrera5000@hotmail.com>
2021-08-30 21:15:08 -0300

    env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBMx9FQQAFXTcVwPgLYquZnvxBkQPE348c",
+    authDomain: "salondejuegos-8dcc9.firebaseapp.com",
+    databaseURL: "https://salondejuegos-8dcc9-default-rtdb.firebaseio.com",
+    projectId: "salondejuegos-8dcc9",
+    storageBucket: "salondejuegos-8dcc9.appspot.com",
+    messagingSenderId: "512465379912",
+    appId: "1:512465379912:web:09d01c6af76e3ce66eee7d",
+    measurementId: "G-FBLHYGJJY2"
+  }
 };
